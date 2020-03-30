cfd072f4ffbe47b0a13fed54584f0c73fe6f8329
aryabhatta <tarun.hive@hotmail.com>
2020-03-30 23:43:34 +0530

    intial commit for SLR

---
+++ Regression/Simple Linear Regression/script.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 30 23:31:56 2020
+
+@author: tarun
+"""
+
+# Simple Linear regression model to estimate the salary of employees based on no. of years of experience
+
