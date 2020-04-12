ab06f7968fc5d6bd5de11854d9bbcd32c6fd480b
aryabhatta <tarun.hive@hotmail.com>
2020-04-12 10:51:43 +0530

    XGBoost returned accuracy of .86 with .01 deviation

--- Model Selection and Boosting/XGBoost/xgb.py
+++ Model Selection and Boosting/XGBoost/xgb.py
@@ -5,3 +5,45 @@ Created on Sun Apr 12 10:24:59 2020
 @author: tarun
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13 ].values    
+y = dataset.iloc[:, 13].values
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])              # X_1 is for country
+
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])              # X_2 is for male/female
+
+ct = ColumnTransformer([("Country", OneHotEncoder(), [1])], remainder = 'passthrough')
+X = ct.fit_transform(X)
+X = X[:, 1:]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# XGBoost
+
+from xgboost import XGBClassifier
+classifier = XGBClassifier()            # parametrers could be change to optimize
+classifier.fit(X_train, y_train)
+
+
+# predictions
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# k fold cross validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X=X_train, y=y_train, cv =10)
+print(accuracies.mean())        # avg accuracy = 86%
+print(accuracies.std())         # std. deviation 1%
