36ea86c163a411b0361de4b82500fd3fc8c6df29
aryabhatta <tarun.hive@hotmail.com>
2020-04-12 08:50:53 +0530

    hyperparameters tuned using GridSearch

--- Model Selection and Boosting/Grid Search/gs.py
+++ Model Selection and Boosting/Grid Search/gs.py
@@ -5,3 +5,46 @@ Created on Sun Apr 12 08:28:36 2020
 @author: tarun
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+dataset = pd.read_csv("Social_Network_Ads.csv")
+X = dataset.iloc[:, [2, 3]].values 
+y = dataset.iloc[:, 4].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test) 
+
+# Kernel SVM
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'rbf', random_state = 0, C = 1000, gamma = 0.1)           # hyperparametrs derived from bestParamters variable of grid search
+classifier.fit(X_train, y_train)
+
+# k-fold cross validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10 ,n_jobs = -1)
+print(accuracies.mean())
+print(accuracies.std())
+
+# Grid search
+
+from sklearn.model_selection import GridSearchCV
+parameters = [ {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
+              {'C': [1, 10, 100, 1000], 'kernel': ['rbf'], 'gamma': [0.5, 0.1, 0.01, 0.001]} ]
+grid_search = GridSearchCV(estimator = classifier, param_grid = parameters, scoring = 'accuracy', cv = 10, n_jobs = -1)          # cv is for k fold cross validation, n_jobs is for cpu number to use
+grid_search = grid_search.fit(X_train, y_train)
+bestAccuracy = grid_search.best_score_
+bestParameters = grid_search.best_params_
+
+        # prediction on test set
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+print(confusion_matrix(y_test, y_pred))
