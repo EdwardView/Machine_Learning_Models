ecc15ce0610da9d4c36b48697f26d38ba51a3e00
aryabhatta <tarun.hive@hotmail.com>
2020-04-12 06:48:12 +0530

    10-fold validation gives avg accuracy of 0.90

--- Model Selection and Boosting/k-Fold Cross Validation/kfcv.py
+++ Model Selection and Boosting/k-Fold Cross Validation/kfcv.py
@@ -5,3 +5,36 @@ Created on Sun Apr 12 06:34:25 2020
 @author: tarun
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+dataset = pd.read_csv("Social_Network_Ads.csv")
+X = dataset.iloc[:, [2, 3]].values 
+y = dataset.iloc[:, 4].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test) 
+
+# Kernel SVM
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'rbf', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# k-fold cross validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10 ,n_jobs = -1)        # cv is number of folds (10 is common)
+print(accuracies.mean())        # gives average accuarcy of model . 90%  here
+print(accuracies.std())         # deviation of 6% 
+
+        # prediction on test set
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+print(confusion_matrix(y_test, y_pred))
